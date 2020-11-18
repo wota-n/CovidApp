a3b0bcbae772649b08a35b3d194c07c4fd61d352
wota-n <najmeenajib99@gmail.com>
2020-11-18 20:31:51 +0800

    added code for retrofitmaincactivity but some nationalDailyData has some issue

---
+++ app/src/main/java/com/inti/covidapp/RetrofitMainActivity.kt
@@ -0,0 +1,52 @@
+package com.inti.covidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val BASE_URL = "https://api.covidtracking.com/v1/"
+private const val TAG = "RetrofitMainActivity"
+
+class RetrofitMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_retrofit_main)
+
+
+        val gson = GsonBuilder().create()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+        val covidService = retrofit.create(CovidService::class.java)
+
+        //Fetch national data
+        covidService.getNationalData().enqueue(object: Callback<List<CovidData>>{
+            override fun onResponse(
+                call: Call<List<CovidData>>,
+                response: Response<List<CovidData>>
+            ) {
+                Log.i(TAG, "onResponse $response")
+                val nationalData = response.body()
+                if(nationalData == null){
+                    Log.w(TAG, "Did not receive a valid response body")
+                    return
+                }
+                nationalDailyData = nationalData
+            }
+
+            override fun onFailure(call: Call<List<CovidData>>, t: Throwable) {
+                Log.e(TAG, "onFailure $t")
+            }
+
+        })
+
+        // Fetch state data
+    }
+}
